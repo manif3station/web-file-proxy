e89820c200e7778a7171a6aa8ded5468bc288a1a
Michael Vu <email@michael.vu>
2022-12-11 04:12:43 +0800

    update

--- src/lib/Web/File/Proxy.pm
+++ src/lib/Web/File/Proxy.pm
@@ -4,34 +4,35 @@ use Dancer2 appname => 'Web';
 
 use Web::Content;
 use HTTP::API::Client;
-use File::Type;
+use File::MimeInfo::Magic;
 
-my $file_type = File::Type->new;
 my $content = Web::Content->new;
 my $api =
   HTTP::API::Client->new( browser_id =>
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
   );
 
-get qr{^/fp/(.+)} {
+get qr{^/fp/(.+)} => sub {
     my ($path) = splat;
+
     $path =~ s/\//./g;
     $path =~ s/\.{2,}/./g;
 
     my $url = $content->get($path);
 
     if ( !$url || $url !~ m{^https?\://} || $url =~ /\s/ ) {
-        statue 'not_found';
+        status 'not_found';
         halt '404 File Not Found';
     }
 
     delayed {
         my $resp = $api->get($url);
         my $content = $resp->decoded_content;
-        response_header "content-type" => do {
+        my $type = do {
             open my $fh, '<', \$content;
-            $file_type->mime_type($fh);
+            mimetype $fh;
         };
+        response_header "content-type" => $type;
         flush;
         content $content;
         done;
